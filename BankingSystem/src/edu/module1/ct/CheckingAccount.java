58db47cd6ac07e1d088b55ec3252491b4129b635
Kristina <Kristina.Farrell@csuglobal.edu>
2024-03-23 20:40:54 -0600

    tests complete

--- BankingSystem/src/edu/module1/ct/CheckingAccount.java
+++ BankingSystem/src/edu/module1/ct/CheckingAccount.java
@@ -1,12 +1,20 @@
  package edu.module1.ct;
 
 public class CheckingAccount extends BankAccount {
-	double intRate;
+	double intRate = 0.05;
 	
 	public void processWithdrawal() {
 		if (balance < 0) {
 			System.out.println("This account has overdrafted. A $30 fee has been appllied. Current balance is now "
 		 + (balance - 30));
 		}
+	}
+	
+	public void displayAccount() {
+		System.out.println("Account ID: " + getAccountID() + 
+	 			"\nBalance: " + getBalance() +
+	 			"\nFirst Name: " + getFirstName() +
+	 			"\nLast Name: " + getLastName());
+		System.out.println("Itnerest Rate: " + intRate +"%");
 	}
 }
--- BankingSystem/src/edu/module1/ct/TestAccount.java
+++ BankingSystem/src/edu/module1/ct/TestAccount.java
@@ -7,9 +7,14 @@ public class TestAccount {
 	public static void main(String[] args) {
 		CheckingAccount newAcct = new CheckingAccount();
 		
+		newAcct.setAccountID(24601);
+		newAcct.setFirstName("Sterling");
+		newAcct.setLastName("Archer");
 		newAcct.deposit();
 		newAcct.withdrawal();
 		newAcct.processWithdrawal();
+		newAcct.displayAccount();
+		
 		
 	
 		

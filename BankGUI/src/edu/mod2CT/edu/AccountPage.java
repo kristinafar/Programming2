48f5ef52a69a7039bd0035eb81625849b109fa91
Kristina <Kristina.Farrell@csuglobal.edu>
2024-03-29 20:51:16 -0600

    adding buttons

--- BankGUI/src/edu/mod2CT/edu/AccountPage.java
+++ BankGUI/src/edu/mod2CT/edu/AccountPage.java
@@ -4,51 +4,57 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 
 public class AccountPage {
-
+	
 	public static void main(String[] args) {
 
-		
-	
+		int balance = 0;
+		int userBal;
 		//make frame
 		JFrame accountPage = new JFrame();
 		accountPage.setTitle("Fells Wargo");
 		accountPage.setSize(500, 500);
 		accountPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		accountPage.setVisible(true);
+		accountPage.setLayout(new FlowLayout());
 		
-		//make layout
-		accountPage.setLayout(new GridBagLayout());
-		GridBagConstraints layoutCon = new GridBagConstraints();
-		layoutCon.gridx = 0;
-		layoutCon.gridy = 0;
-		layoutCon.insets = new Insets(10, 10, 10, 10);
 		
-		JLabel curBalance = new JLabel("Account Balance: ");
+		//make input and buttons
+		JLabel curBalance = new JLabel("Enter Account Balance: ");
+		JTextField balField = new JTextField(8);
+		balField.setEditable(true);
+		balField.setText("Enter amount");
+		JButton depButton = new JButton("Deposit");
+		JButton withButton = new JButton("Withdraw");
+		JButton balButton = new JButton("Current Balance");
 		
-		JTextField balField = new JTextField(5);
-		balField.setEditable(false);
-		balField.setText("0");
+		
+		depButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+			curBalance.setText("Enter Deposit Amount: ");
+			
+		}
+	});
+		
+	
 		
 		
 		//make panels
 		JPanel accountBalance = new JPanel();
 		
-		  layoutCon = new GridBagConstraints();
-		  layoutCon.gridx = 1;
-		  layoutCon.gridy = 0;
-		  layoutCon.insets = new Insets(10, 10, 10, 10);
-		  accountPage.add(curBalance, layoutCon);
-		
-		  layoutCon = new GridBagConstraints();
-		  layoutCon.gridx = 0;
-		  layoutCon.gridy = 1;
-		  layoutCon.insets = new Insets(10, 10, 10, 10);
-		  accountPage.add(balField, layoutCon);
+		 accountPage.add(curBalance);
+		 accountPage.add(balField);
+		 accountPage.add(depButton);
+		 accountPage.add(withButton);
+		 accountPage.add(balButton);
+		 
 		
 		
 		
